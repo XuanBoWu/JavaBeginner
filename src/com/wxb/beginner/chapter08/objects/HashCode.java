4c4672844d28e95a8d8998be28ad06da99bf2b03
XuanBoWu <wuxuanbo@foxmail.com>
2022-03-14 22:37:33 +0800

    add video 322~327 code

---
+++ src/com/wxb/beginner/chapter08/objects/HashCode.java
@@ -0,0 +1,18 @@
+package com.wxb.beginner.chapter08.objects;
+
+public class HashCode {
+    public static void main(String[] args) {
+        AA aa1 = new AA();
+        AA aa2 = new AA();
+
+        System.out.println(aa1.hashCode());
+        System.out.println(aa2.hashCode());
+        AA aa3 = aa1;
+        System.out.println(aa3.hashCode());
+
+    }
+}
+
+class AA {
+
+}
---
+++ src/com/wxb/beginner/chapter08/objects/ToString.java
@@ -0,0 +1,33 @@
+package com.wxb.beginner.chapter08.objects;
+
+public class ToString {
+    public static void main(String[] args) {
+        Monster jinx = new Monster("Jinx", "shooting", 13333);
+
+        System.out.println(jinx.toString());
+        System.out.println(jinx);
+
+    }
+}
+
+
+class Monster {
+    String name;
+    String job;
+    double salary;
+
+    @Override
+    public String toString() {
+        return "Monster{" +
+                "name='" + name + '\'' +
+                ", job='" + job + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public Monster(String name, String job, double salary) {
+        this.name = name;
+        this.job = job;
+        this.salary = salary;
+    }
+}
