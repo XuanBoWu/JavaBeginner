598caf0dd1d5cce7366cb0a615d18d24ff37892e
XuanBoWu <wuxuanbo@foxmail.com>
2022-03-30 01:38:40 +0800

    add video 360~372 code：Housing rental management system

---
+++ src/com/wxb/beginner/chapter09/house_rent/HouseRentApp.java
@@ -0,0 +1,10 @@
+package com.wxb.beginner.chapter09.house_rent;
+
+import com.wxb.beginner.chapter09.house_rent.domian.House;
+import com.wxb.beginner.chapter09.house_rent.view.HouseView;
+
+public class HouseRentApp { // Program entry
+    public static void main(String[] args) {
+        new HouseView().mainView();
+    }
+}
