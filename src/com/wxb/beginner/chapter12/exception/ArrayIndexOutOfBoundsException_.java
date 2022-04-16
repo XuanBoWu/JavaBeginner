139d38e513b64be67769a5d2eb598a18af3dcc79
XuanBoWu <wuxuanbo@foxmail.com>
2022-04-16 16:53:22 +0800

    add video 442~458 code

---
+++ src/com/wxb/beginner/chapter12/exception/ArrayIndexOutOfBoundsException_.java
@@ -0,0 +1,14 @@
+package com.wxb.beginner.chapter12.exception;
+
+/**
+ * @author alex
+ * @version 1.0
+ */
+public class ArrayIndexOutOfBoundsException_ {
+    public static void main(String[] args) {
+
+        int[] p = {1, 2, 3};
+
+        System.out.println(p[3]);
+    }
+}
